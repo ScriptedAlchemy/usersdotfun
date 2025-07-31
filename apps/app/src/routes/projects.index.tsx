f0d76bb8e06c9536bcb76407e197f20efa0256b0
Elliot Braem <elliot@ejlbraem.com>
2025-07-31 18:41:36 -0500

    clean up

--- apps/app/src/routes/projects.index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-export const Route = createFileRoute('/projects/')({
-  component: ProjectsIndexComponent,
-})
-
-function ProjectsIndexComponent() {
-  return <div>Select a project.</div>
-}
